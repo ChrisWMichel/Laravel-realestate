bd0ae51bfb2327a6f4ee39f14067beaf316c0cba
ChrisWMichel <christopherw.michel@gmail.com>
2025-01-09 14:16:34 -0600

    update 2

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,8 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 //use App\Http\Middleware\VerifyCsrfToken;
+use Litespeed\LSCache\LSCacheMiddleware;
+use Litespeed\LSCache\LSTagsMiddleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -15,6 +17,8 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->web(append: [
             \App\Http\Middleware\HandleInertiaRequests::class,
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
+            LSCacheMiddleware::class,
+            LSTagsMiddleware::class,
         ]);
 
         $middleware->validateCsrfTokens(except: [
