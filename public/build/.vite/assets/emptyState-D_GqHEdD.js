6fc3ac47bac1251683eb51de6c675c5f36dc47b7
ChrisWMichel <christopherw.michel@gmail.com>
2025-03-04 11:03:45 -0600

    adding build directory

---
+++ public/build/.vite/assets/emptyState-D_GqHEdD.js
@@ -0,0 +1,1 @@
+import{B as e}from"./BoxShadow-C0PT3EVi.js";import{o as a,j as o,w as s,b as r,y as c}from"./app-DZuH0l-K.js";const l={class:"w-full font-medium text-center text-gray-400"},p={__name:"emptyState",setup(n){return(t,_)=>(a(),o(e,null,{default:s(()=>[r("div",l,[c(t.$slots,"default")])]),_:3}))}};export{p as _};
